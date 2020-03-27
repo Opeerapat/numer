f3a40b79c60058015d4b6de48e6540acdf477a4d
Opeerapat <himaxnmo12345a@hotmail.com>
2020-03-27 15:13:43 +0700

    update13

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.99.100:8080/api',
-    //baseURL: 'http://localhost:8080/api'
+    //baseURL: 'http://192.168.99.100:8080/api',
+    baseURL: 'http://localhost:8080/api'
 })
 
 
